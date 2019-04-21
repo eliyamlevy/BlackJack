eb50b8c8b2f9dcb3e3d9c01aa478114688ca032d
Skolbot <stewsmith114@gmail.com>
2019-04-20 22:33:08 -0700

    Added different gamemode support/polished win conditions

--- src/Server/TableThread.java
+++ src/Server/TableThread.java
@@ -13,6 +13,7 @@ public class TableThread extends Thread{
 	private Vector<Integer> deck;
 	private Random r;
 	private Vector<Integer> scores;
+	private boolean modeJustDealer;
 	
 	public Boolean getRoundStatus() {
 		System.out.println("Getting round status: " + inRound);
@@ -27,11 +28,12 @@ public class TableThread extends Thread{
 		return players.contains(pt);
 	}
 	
-	public TableThread(PlayerThread opt, int max) {
+	public TableThread(PlayerThread opt, int max,boolean justDealer) {
 		owner = opt;
 		owner.SetReady(true); //when owner creates table they are automatically ready
 		inRound = false;
 		this.maxPlayers = max;
+		modeJustDealer  = justDealer;
 		players.add(opt);
 		r = new Random();
 		deck = new Vector<Integer>();
@@ -95,11 +97,18 @@ public class TableThread extends Thread{
 			
 			DealerAI dealer = new DealerAI();
 			dealer.hand = new Vector<Integer>();
+			//Check if dealer has Blackjack
+			boolean dealerBlackJack = false;
 			
 			System.out.println("Dealer's cards dealt.");
 			
 			dealer.hand.add(BlackJackHelpers.DealCard(deck,r));
 			dealer.hand.add(BlackJackHelpers.DealCard(deck,r));
+			
+			if(BlackJackHelpers.GetIdealScore(dealer.hand)==21)
+			{
+				dealerBlackJack = true;
+			}
 			
 			for (int i = 0; i<players.size(); i++) {
 				players.get(i).setTurn(true);
@@ -122,45 +131,159 @@ public class TableThread extends Thread{
 			}
 			
 			System.out.println("Dealer's turn.");
-			dealer.hitorstay(deck, r);
+			int dscore = 0;
+			if(!dealerBlackJack)
+			{
+				dealer.hitorstay(deck, r);
+				dscore = BlackJackHelpers.GetIdealScore(dealer.hand);
+				System.out.println("Dealer got "+dscore+".");
+				
+			}
+			else
+			{
+				dscore = 21;
+				System.out.println("Dealer got a blackjack!");
+			}
 			
 			System.out.println("Everyone has finished. Calculating results.");
-			int dscore = BlackJackHelpers.GetIdealScore(dealer.hand);
-			for(int i=0; i<players.size();i++)
+			if(modeJustDealer)
 			{
-				int curBal = players.get(i).getBalance();
-				int curBet = players.get(i).getBet();
-				int pscore = scores.get(i);
-				System.out.println("Player "+(i+1)+":");
-				if(players.get(i).getBlackjack() == true)
+				System.out.println("Everyone against dealer");
+				for(int i=0; i<players.size();i++)
 				{
-					System.out.println(players.get(i).username+" got a blackjack!  Win.");
-					players.get(i).setBalance(curBal+(2*curBet));
+					int curBal = players.get(i).getBalance();
+					int curBet = players.get(i).getBet();
+					int pscore = scores.get(i);
+					System.out.println("Player "+(i+1)+":");
+					if(pscore>21)
+					{
+						System.out.println(players.get(i).username+" has busted ("+pscore+"). Loss.");
+					}
+					else if (dscore == pscore)
+					{
+						System.out.println("Dealer ("+dscore+") tied with "+players.get(i).username+" ("+pscore+"). Push.");
+						players.get(i).setBalance(curBal+(curBet));
+					}
+					else if(players.get(i).getBlackjack() == true)
+					{
+						System.out.println(players.get(i).username+" got a blackjack!  Win.");
+						players.get(i).setBalance(curBal+(2*curBet));
+					}
+					else if(dscore>21)
+					{
+						System.out.println("Dealer ("+dscore+") busted, so "+players.get(i).username+" Wins.");
+						players.get(i).setBalance(curBal+(2*curBet));
+					}
+					else if(pscore<dscore)
+					{
+						System.out.println("Dealer ("+dscore+") beat "+players.get(i).username+" ("+pscore+"). Loss.");
+					}
+					else if(pscore>dscore)
+					{
+						System.out.println("Dealer ("+dscore+") lost to "+players.get(i).username+" ("+pscore+"). Win.");
+						players.get(i).setBalance(curBal+(2*curBet));
+					}
 				}
-				else if(pscore>21)
+			}	
+			//Pooled mode - highest player takes all, ties are split between players (including dealer)
+			else if(!modeJustDealer)
+			{
+				System.out.println("Pooled prize.");
+				int pooledPrize = 0;
+				int numWinners = 0;
+				Vector<Integer> winners = new Vector<Integer>();
+				for(int i=0; i<players.size();i++)
 				{
-					System.out.println(players.get(i).username+" has busted ("+pscore+"). Loss.");
+					pooledPrize += players.get(i).getBet();
+					//Check for players with blackjacks and store the IDs.
+					if(players.get(i).getBlackjack())
+					{
+						winners.add(i);
+					}
 				}
-				else if(dscore>21)
+				//If players were found with blackjacks
+				if(!winners.isEmpty())
 				{
-					System.out.println("Dealer ("+dscore+") busted, so "+players.get(i).username+" Wins.");
-					players.get(i).setBalance(curBal+(2*curBet));
+					//All those players are winners
+					for(int i : winners)
+					{
+						System.out.println(players.get(i).username+" wins!");
+						numWinners++;
+					}
+					//If dealer also got a blackjack
+					if(dealerBlackJack)
+					{
+						System.out.println("Dealer wins!");
+						numWinners++;
+					}
+					System.out.println("Prize pool divided amongst winners.");
+					for(int i : winners)
+					{
+						players.get(i).setBalance((int)(pooledPrize/numWinners));
+					}
 				}
-				else if(pscore<dscore)
+				//Nobody got a blackjack
+				else
 				{
-					System.out.println("Dealer ("+dscore+") beat "+players.get(i).username+" ("+pscore+"). Loss.");
+					//Dealer has a blackjack, only winner
+					if(dealerBlackJack)
+					{
+						System.out.println("Dealer wins!");
+					}
+					//No blackjacks
+					else
+					{
+						//Calculating highest score
+						int highestScore = dscore;
+						//Compare player scores to dealer score
+						for(int i = 0; i<scores.size();i++)
+						{
+							//If higher than current highest
+							//Set highest to that
+							//clear winner vector
+							//Add player index to winner vector
+							if(scores.get(i)>highestScore)
+							{
+								highestScore = scores.get(i);
+								winners.clear();
+								winners.add(i);
+							}
+							//if tied, just add player index to winner vector
+							else if(scores.get(i)==highestScore)
+							{
+								winners.add(i);
+							}
+						}
+						//if winner vector is empty then dealer is only winner
+						if(winners.isEmpty())
+						{
+							System.out.println("Dealer wins!");
+						}
+						//If winner vector is not empty then at least one player won
+						else if(!winners.isEmpty())
+						{
+							for(int i : winners)
+							{
+								System.out.println(players.get(i).username+" wins!");
+								numWinners++;
+							}
+							//if highest score is still equal to dealer score then dealer is also a winner
+							if(dscore==highestScore)
+							{
+								System.out.println("Dealer wins!");
+								numWinners++;
+							}
+							System.out.println("Prize pool divided amongst winners.");
+							for(int i : winners)
+							{
+								players.get(i).setBalance((int)(pooledPrize/numWinners));
+							}
+							
+						}
+					}
 				}
-				else if(pscore>dscore)
-				{
-					System.out.println("Dealer ("+dscore+") lost to "+players.get(i).username+" ("+pscore+"). Win.");
-					players.get(i).setBalance(curBal+(2*curBet));
-				}
-				else if (dscore == pscore)
-				{
-					System.out.println("Dealer ("+dscore+") tied with "+players.get(i).username+" ("+pscore+"). Push.");
-					players.get(i).setBalance(curBal+(curBet));
-				}
-			}
+			
+			}	
 			
 			System.out.println("TableThread: Round over, resetting.");
 			inRound = false;
